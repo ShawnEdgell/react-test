197d3972a3210ad3d47ddc598323cd06071f0fb5
shawn edgell <shawn_edgell@hotmail.com>
2023-10-18 02:01:19 -0400

    added first react component

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,29 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react()
+  ],
+  base: '/weather-app-2/',
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  build: {
+    target: 'esnext', 
+    outDir: 'dist', 
+    minify: 'terser',
+    sourcemap: true
+  },
+  server: {
+    port: 3000, 
+    open: true,   
+    strictPort: false, 
+    cors: true,   
+    https: false,  
+    proxy: { 
+    }
+  },
+  
+});
+
